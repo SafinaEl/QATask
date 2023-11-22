d16c29a7ec527d293e6f3d847c06bc931fba1b5c
Эльвира Сафина <elvirasafina@MBP-Elvira.git>
2023-11-22 22:30:06 +0500

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) { System.out.println(multiplicationCase(5, 6));
+    }
+
+    public static int multiplicationCase(int a, int c){
+        return a * c;
+    }
+}
